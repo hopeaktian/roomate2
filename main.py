43441bab4e9e5ed45b2afc762ee27e0cd63f128a
hopeaktian <hopeaktian@gmail.com>
2018-08-06 12:11:27 +0800

    add error page.

--- main.py
+++ main.py
@@ -89,6 +89,14 @@ class Criticism(db.Model):
 def index():
     return render_template('index2.html', title="主页")
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('error.html'), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('error.html'), 500
+
 @app.route('/messagewall', methods=['GET', 'POST'])
 def messagewall():
     global success
